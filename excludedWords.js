09c072c1ba8026e81eecae1aab6daa0f7ce9acd8
Tyler Seaberg <czackdesigns@gmail.com>
2020-09-09 00:07:30 -0400

    testing performance with server only providing exact information rather than all comments

---
+++ excludedWords.js
@@ -0,0 +1,3 @@
+const excludedWords = ['this', 'the', 'a', 'is', 'i', 'of', 'to', 'in', 'and', 'that', 'for', 'are', 'it', 'was', 'has', 'with', 'if', 'or', 'but', 'what', 'so', 'by', 'as', 'an']
+
+module.exports.excludedWords = excludedWords
--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const ip = process.env.IP || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 //Import puppeteer function
-const {redditScraper} = require('./scraper');
+const { redditScraper } = require('./scraper');
+
+//Import constant
+const { excludedWords } = require('./excludedWords')
 
 //Allows CORS
 app.use((req, res, next) => {
@@ -17,12 +20,55 @@ app.use((req, res, next) => {
 app.get('/search', (request, response) => {
 
     //Holds value of the query param 'searchquery'.
-    const searchQuery = request.query.searchquery;
+    const searchQuery = request.query.searchquery.split('-');
 
     //Do something when the searchQuery is not null.
     if (searchQuery != null) {
 
-        redditScraper(searchQuery, 5)
+        redditScraper(searchQuery[0], searchQuery[1])
+            .then(results => {
+                const setWordCount = (comments) => {
+                    let words = []
+                    comments.forEach((comment) => {
+                        words.push(comment.commentText.toLowerCase().split(' '))
+                    })
+            
+                    let wordCount = []
+                    words.forEach((arrOfWords) => {
+                        arrOfWords.forEach((word) => {
+                            wordCount.push(word.trim().toLowerCase())
+                        })
+                    })
+            
+                    let matches = []
+                    wordCount.forEach((word) => {
+                        !matches.includes(word) && !excludedWords.includes(word) && matches.push(word)
+                    })
+                    
+                    let matchCount = []
+                    matches.forEach((match) => {
+                        matchCount.push({
+                            word: match,
+                            wordCount: 0
+                        })
+                    })
+            
+                    matchCount.forEach((word) => {
+                        wordCount.forEach((match) => {
+                            word.word === match && word.wordCount++
+                        })
+                    })
+                    
+                    matchCount.sort((a, b) => {
+                        return b.wordCount - a.wordCount
+                    })
+            
+                    matchCount.splice(50)
+            
+                    return matchCount       
+                }
+                return setWordCount(results)
+            })
             .then(results => {
                 //Returns a 200 Status OK with Results JSON back to the client.
                 response.status(200);

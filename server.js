fc4df0eefa890d44d663cb58c1e0e3b0de1bf536
Tyler Seaberg <czackdesigns@gmail.com>
2021-01-18 13:11:03 -0500

    fixing cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { redditScraper } = require('./scraper');
 
 //Allows CORS
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin: *");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8000/");
     next();
   })
   

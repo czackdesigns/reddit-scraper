2bede1119f8cf8782078ee5efbc7a6dfa9c9bad0
Tyler Seaberg <czackdesigns@gmail.com>
2021-01-20 13:53:13 -0500

    trying to fix cors error

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const { redditScraper } = require('./scraper');
 //Allows CORS
 app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
   });
   
